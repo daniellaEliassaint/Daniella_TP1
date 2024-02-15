457c56c8fbcc5a6d9a651cf41954b5a5449a5866
daniellaEliassaint <2241176@cmontmorency.qc.ca>
2024-02-14 23:10:02 -0500

    premiere partie

--- module1.py
+++ module1.py
@@ -1,6 +1,49 @@
+import datetime
+import json 
+
+#création de la classe/objet Joueur 
+class Joueur :
+    
+    #constructeur
+    def __init__(self, p_nomJoueur ):
+       self.datePartie =  datetime.datetime.now()
+       self.nomJoueur = p_nomJoueur
+       self.listeReponses = {}
+       self.pointage = 0
+       
+    #methode pour afficher les données
+    def afficherPartie(self):
+        print("Date: " +str(self.datePartie) + ",Nom: " + self.nomJoueur +
+              ",listeReponses: " + str(self.listeReponses) +
+              "Pointage: " + str(self.pointage))
+        
+#Affichage ddes règles du jeu
 print("|-------------------------------------------------------------|")
 print("|   Travail pratique 1 - Quiz 4B5 - Daniella Eliassaint       |")
 print("|-------------------------------------------------------------|")
+print("|  Choississez la bonne réponse pour chaque question (a/b/c)  |")
+print("|  q pour quitter                                             |")
+print("|-------------------------------------------------------------|")
+
+nom = input("Entrez votre nom: ")
+joueur = Joueur(nom)
+
+joueur.afficherPartie()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
