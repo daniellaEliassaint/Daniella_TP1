423e0f3e0dc17632fdfedfda1c12953c67ad983d
daniellaEliassaint <2241176@cmontmorency.qc.ca>
2024-02-20 17:52:24 -0500

    Partie terminée ert corriger

--- module1.py
+++ module1.py
@@ -8,7 +8,7 @@ class Joueur :
     def __init__(self, p_nomJoueur ):
        self.datePartie =  datetime.datetime.now()
        self.nomJoueur = p_nomJoueur
-       self.listeReponses = {}
+       self.listeReponses = []
        self.pointage = 0
        
     #methode pour afficher les données
@@ -17,8 +17,8 @@ class Joueur :
               ",listeReponses: " + str(self.listeReponses) +
               "Pointage: " + str(self.pointage))
         
-with open('questions.json', 'r') as fic:
-    questions = json.load(fic)
+with open('questions.json', 'r',encoding="utf-8") as fic:
+   questions = json.load(fic)
         
 #Affichage ddes règles du jeu
 print("|-------------------------------------------------------------|")
@@ -30,19 +30,51 @@ print("|-------------------------------------------------------------|")
 
 nom = input("Entrez votre nom: ")
 joueur = Joueur(nom)
-
 numeroQuestion =1
-
+#afficher les questions
 for i in questions:
-    print(numeroQuestion,":",i["q"])
-    print("a.",i["a"])
-    print("b.",i["b"])
-    print("c.",i["c"])
+    print(numeroQuestion,")",i["q"])
+    print("<a>",i["a"])
+    print("<b>",i["b"])
+    print("<c>",i["c"])
     
-    choix = input("Réponse (a/b/c/q): ")
+    choix = input("Entrez l'option (a/b/c) ou q pour quitter: ").lower()
+    
+    if choix == i["rep"]:
+        print ("Bonne réponse!")
+        joueur.pointage += i["pts"]
+        reponse = True
 
+    elif choix == 'q':
+        break
+    else :
+        print ("Mauvaise réponse. La bonne réponse est " , i["rep"])
+        reponse=False
+        
+    joueur.listeReponses.append( {"question": i["q"],
+                                  "reponse": choix,
+                                  "bonne" :reponse})
+    
+    print()
+    numeroQuestion +=1
+  
+joueur.afficherPartie()
+    
+resultat_dict = {
+    "nom": joueur.nomJoueur,
+    "datePartie": str(joueur.datePartie),
+    "listeReponses": joueur.listeReponses,
+    "pointage": joueur.pointage
+}
 
-    numeroQuestion  += 1
+# écriture du dictionnaire dans un fichier 
+with open ("resultat.json", "w", encoding="utf-8") as fic_json:
+    json.dump(resultat_dict,
+              fic_json,
+              ensure_ascii=False,
+              indent = 4,
+              sort_keys=False)
+
 
 
 
