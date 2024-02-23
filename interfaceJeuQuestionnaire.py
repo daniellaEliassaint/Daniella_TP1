13295619af114011d5a69902ce73f619dbd6abc8
daniellaEliassaint <2241176@cmontmorency.qc.ca>
2024-02-22 23:18:33 -0500

    Partie 2 terminée

--- interfaceJeuQuestionnaire.py
+++ interfaceJeuQuestionnaire.py
@@ -26,37 +26,32 @@ class fenetre(tk.Tk):
         #label pour la date
         self.date_label = tk.Label(self, text="Sélectionnez la date de la partie:")
         self.date_label.pack()
-
+        
+        self.date_combobox = ttk.Combobox( width=30)
+        self.date_combobox.pack()
+        
         #label pour le nom du joueur
         self.joueur_label = tk.Label(self, text="Sélectionnez le nom du joueur:")
         self.joueur_label.pack()
         
+         #ajout comboBox : utiliser le module ttk de tkinker
+      
+        self.joueur_combo = ttk.Combobox(width=30)
+        self.joueur_combo.pack()
+        
         #label pour les reponses du joueur
-        self.reponse_label = tk.Label(self, text="Sélectionnez la date de la partie:")
+        self.reponse_label = tk.Label(self, text="Résultat du joueur:")
         self.reponse_label.pack()
         
-        #label pour afficher le pointage
-        self.pointage_label = tk.Label(self, text="Pointage du joueur:", font= ("Helvetica", 10))
-        self.pointage_label.pack()
-        
-        #label du nbr de points total du joueur
-        self.score_label = tk.Label(self, text="",font= ("Helvetica", 12))
-        self.score_label.pack()
         
         #champ texte
         self.champ_text = tk.Text(self, height=10, width=60)
         self.champ_text.pack()
         
-       #cadre haut
-        self.frm_haut = tk.Frame()
-        self.frm_haut.pack(side=tk.TOP) 
         
-         #ajout comboBox : utiliser le module ttk de tkinker
-        self.date_combobox = ttk.Combobox( width=30)
-        self.date_combobox.pack()
-        self.joueur_combo = ttk.Combobox(width=30)
-        self.joueur_combo.pack()
-
+        #label du nbr de points total du joueur
+        self.score_label = tk.Label(self, text="",font= ("Helvetica", 12))
+        self.score_label.pack()
 
  #importer un fichier Json       
     def btn_importer_clicked(self):
@@ -83,15 +78,32 @@ class fenetre(tk.Tk):
  #Selectionner un joueur
     def joueur(self, event):
         date_select = self.date_combobox.get()
-        fichier_select = next((resultat for resultat in self.resultats if resultat["datePartie" ]== date_select), None)
+        fichier_select = next((resultat for resultat in self.resultats if resultat["datePartie" ] == date_select), None)
         
         if fichier_select:
             self.joueur_combo['values'] = [fichier_select.get("nom", "")]
             self.joueur_combo.set(fichier_select.get("nom",""))
-            self.joueur_combo.bind("<<<ComboboxSelected>>>", lambda event, date= fichier_select:self.afficherReponse(event, date))
+            self.joueur_combo.bind("<<ComboboxSelected>>", lambda event, date= fichier_select:self.afficherReponse(event, date))
  
  # selectionner le pointage et le resultat
     def afficherReponse(self, event, fichier_select):
+        self.champ_text.delete(1.0, tk.END)
+        select_resultat = fichier_select
+        
+        for reponse in select_resultat.get("listeReponses", ()):
+            question = "{} Réponse: {}".format(reponse.get('question', ''), reponse.get('reponse', '')) 
+            correct = reponse.get("bonne", False)
+            
+            couleur = "green" if correct else "red"
+            self.champ_text.insert(tk.END, question, couleur)
+            
+            debut = self.champ_text.index(tk.END) + "-2c"
+            fin = self.champ_text.index(tk.END)
+            self.champ_text.tag_add(couleur,debut, fin)
+            
+            self.champ_text.tag_config(couleur, foreground=couleur) 
+            
+        self.score_label.config(text="Pointage: {}".format(select_resultat.get('pointage', 0)))
             
     #programme principal
 if __name__ =="__main__":
